25cef1f419604fa26f27a79b12c17412e3bb152f
felixalguzman <felixlvl@gmail.com>
2017-06-25 10:05:22 -0400

    Lectura de archivo

--- Weka Project/src/logical/Prueba.java
+++ Weka Project/src/logical/Prueba.java
@@ -1,7 +1,24 @@
 package logical;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import weka.core.Instances;
+
 public class Prueba {
 
-	
-	
+
+
+
+
+	public void Leer() throws IOException
+	{
+		BufferedReader reader = new BufferedReader( new FileReader("/some/where/data.arff"));
+		 Instances data = new Instances(reader);
+		 reader.close();
+		 data.setClassIndex(data.numAttributes() - 1);
+	}
+
 }
